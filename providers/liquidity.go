bf38ba325ca626c96dbb2781a6a92f7f65f5d6d4
aguschirico <aguschirico@gmail.com>
2021-08-04 13:37:34 +0200

    add missing param to signature

--- providers/liquidity.go
+++ providers/liquidity.go
@@ -20,7 +20,7 @@ import (
 type LiquidityProvider interface {
 	GetQuote(types.Quote, uint64, big.Int) *types.Quote
 	Address() string
-	SignHash() []byte
+	SignHash(hash []byte) []byte
 }
 
 type LocalProvider struct {
