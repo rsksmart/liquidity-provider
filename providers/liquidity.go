398368afb10460d4364e59690133c8154525bfdf
aguschirico <aguschirico@gmail.com>
2021-08-04 13:41:12 +0200

    return error to caller

--- providers/liquidity.go
+++ providers/liquidity.go
@@ -20,7 +20,7 @@ import (
 type LiquidityProvider interface {
 	GetQuote(types.Quote, uint64, big.Int) *types.Quote
 	Address() string
-	SignHash(hash []byte) []byte
+	SignHash(hash []byte) ([]byte, error)
 }
 
 type LocalProvider struct {
