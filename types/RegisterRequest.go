98e844575e5b2e9d303d7e2842c860bdc8d44cc0
Dominik Z <66622718+Dominikkq@users.noreply.github.com>
2023-03-10 15:26:16 +0200

    OpenAPI Specifications on RegisterRequest.go

--- types/RegisterRequest.go
+++ types/RegisterRequest.go
@@ -1,12 +1,12 @@
 package types
 
 type ProviderRegisterRequest struct {
-    Name             string `json:"name"`
-    Fee              uint   `json:"fee"`
-    QuoteExpiration  uint   `json:"quoteExpiration"`
-    AcceptedQuoteExpiration uint `json:"acceptedQuoteExpiration"`
-    MinTransactionValue uint `json:"minTransactionValue"`
-    MaxTransactionValue uint `json:"maxTransactionValue"`
-    ApiBaseUrl       string `json:"apiBaseUrl"`
-    Status           bool   `json:"status"`
+    Name             string `json:"name" example:"New Provider" description:"Provider Name"`
+    Fee              uint   `json:"fee" example:"100" description:"Fee in wei"`
+    QuoteExpiration  uint   `json:"quoteExpiration" example:"100" description:"Quote expiration in seconds"`
+    AcceptedQuoteExpiration uint `json:"acceptedQuoteExpiration" example:"100" description:"Accepted quote expiration in seconds"`
+    MinTransactionValue uint `json:"minTransactionValue" example:"100" description:"Minimum transaction value in wei"`
+    MaxTransactionValue uint `json:"maxTransactionValue" example:"100" description:"Maximum transaction value in wei"`
+    ApiBaseUrl       string `json:"apiBaseUrl" example:"https://api.example.com" description:"API base URL"`
+    Status           bool   `json:"status" example:"true" description:"Provider status"`
 }
