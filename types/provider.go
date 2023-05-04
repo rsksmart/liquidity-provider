7ae7a7d04a91b79195ae57e36502c293cc065e60
Dominik Z <66622718+Dominikkq@users.noreply.github.com>
2023-05-04 18:26:22 +0300

    Update provider.go

--- types/provider.go
+++ types/provider.go
@@ -4,9 +4,9 @@ type GlobalProvider struct {
     Id               uint64 `json:"id" example:"1" description:"Provider Id"`
     Provider         string `json:"provider" example:"0x0" description:"Provider Address"`
     Name             string `json:"name" example:"New Provider" description:"Provider Name"`
-    Fee              uint   `json:"fee" example:"100" description:"Fee in wei"`
-    QuoteExpiration  uint   `json:"quoteExpiration" example:"100" description:"Quote expiration in seconds"`
-    AcceptedQuoteExpiration uint `json:"acceptedQuoteExpiration" example:"100" description:"Accepted quote expiration in seconds"`
+    Fee              uint64   `json:"fee" example:"100" description:"Fee in wei"`
+    QuoteExpiration  uint64   `json:"quoteExpiration" example:"100" description:"Quote expiration in seconds"`
+    AcceptedQuoteExpiration uint64 `json:"acceptedQuoteExpiration" example:"100" description:"Accepted quote expiration in seconds"`
     MinTransactionValue uint64 `json:"minTransactionValue" example:"100" description:"Minimum transaction value in wei"`
     MaxTransactionValue uint64 `json:"maxTransactionValue" example:"100" description:"Maximum transaction value in wei"`
     ApiBaseUrl       string `json:"apiBaseUrl" example:"https://api.example.com" description:"API base URL"`
