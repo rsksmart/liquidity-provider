d874351505d75ed9f4bbaac63fae2969c94b2492
Dominik Z <66622718+Dominikkq@users.noreply.github.com>
2023-05-17 15:08:04 +0300

    Create UserEvents.go

---
+++ types/UserEvents.go
@@ -0,0 +1,11 @@
+package types
+
+type UserEvents struct {
+	From      common.Address `json:"from" example:"0x0" description:"From Address"`
+	Dest      common.Address `json:"dest" example:"0x0" description:"Destination Address"`
+	GasLimit  *big.Int 		 `json:"gasLimit" example:"10000" description:"Gas Limit"`
+	Value     *big.Int       `json:"value" example:"10000" description:"Event Value"`
+	Data      []byte         `json:"data" example:"[]" description:"Event Data"`
+	Success   bool			 `json:"success" example:"true" description:"Event Status"`
+	QuoteHash [32]byte		 `json:"quoteHash" example:"0x0" description:"QuoteHash"`
+}
