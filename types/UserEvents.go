993ee1949b1857e07c31af4ee0328cb0d4c060df
Dominik Z <66622718+Dominikkq@users.noreply.github.com>
2023-05-17 16:04:38 +0300

    Update UserEvents.go

--- types/UserEvents.go
+++ types/UserEvents.go
@@ -8,9 +8,9 @@ import (
 type UserEvents struct {
 	From      common.Address `json:"from" example:"0x0" description:"From Address"`
 	Dest      common.Address `json:"dest" example:"0x0" description:"Destination Address"`
-	GasLimit  *big.Int 		   `json:"gasLimit" example:"10000" description:"Gas Limit"`
+	GasLimit  *big.Int       `json:"gasLimit" example:"10000" description:"Gas Limit"`
 	Value     *big.Int       `json:"value" example:"10000" description:"Event Value"`
 	Data      []byte         `json:"data" example:"[]" description:"Event Data"`
-	Success   bool			     `json:"success" example:"true" description:"Event Status"`
-	QuoteHash [32]byte		   `json:"quoteHash" example:"0x0" description:"QuoteHash"`
+	Success   bool	         `json:"success" example:"true" description:"Event Status"`
+	QuoteHash string         `json:"quoteHash" example:"0x0" description:"QuoteHash"`
 }
