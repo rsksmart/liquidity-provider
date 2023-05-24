915baafdc3e6b4da48f4cd80bb24bdb7e30ae417
Dominik Z <66622718+Dominikkq@users.noreply.github.com>
2023-05-24 20:36:08 +0300

    Update RegisterRequest.go

--- types/RegisterRequest.go
+++ types/RegisterRequest.go
@@ -1,12 +1,13 @@
 package types
 
 type ProviderRegisterRequest struct {
-    Name             string `json:"name" example:"New Provider" description:"Provider Name"`
-    Fee              uint   `json:"fee" example:"100" description:"Fee in wei"`
-    QuoteExpiration  uint   `json:"quoteExpiration" example:"100" description:"Quote expiration in seconds"`
-    AcceptedQuoteExpiration uint `json:"acceptedQuoteExpiration" example:"100" description:"Accepted quote expiration in seconds"`
-    MinTransactionValue uint64 `json:"minTransactionValue" example:"100" description:"Minimum transaction value in wei"`
-    MaxTransactionValue uint64 `json:"maxTransactionValue" example:"100" description:"Maximum transaction value in wei"`
-    ApiBaseUrl       string `json:"apiBaseUrl" example:"https://api.example.com" description:"API base URL"`
-    Status           bool   `json:"status" example:"true" description:"Provider status"`
+    Name             string `json:"name" bson:"name" example:"New Provider" description:"Provider Name"`
+    Fee              uint   `json:"fee" bson:"fee" example:"100" description:"Fee in wei"`
+    QuoteExpiration  uint   `json:"quoteExpiration" bson:"quoteExpiration" example:"100" description:"Quote expiration in seconds"`
+    AcceptedQuoteExpiration uint `json:"acceptedQuoteExpiration" bson:"acceptedQuoteExpiration"  example:"100" description:"Accepted quote expiration in seconds"`
+    MinTransactionValue uint64 `json:"minTransactionValue" bson:"minTransactionValue" example:"100" description:"Minimum transaction value in wei"`
+    MaxTransactionValue uint64 `json:"maxTransactionValue" bson:"maxTransactionValue" example:"100" description:"Maximum transaction value in wei"`
+    ApiBaseUrl       string `json:"apiBaseUrl" bson:"apiBaseUrl" example:"https://api.example.com" description:"API base URL"`
+    Status           bool   `json:"status" bson:"status" example:"true" description:"Provider status"`
 }
+
