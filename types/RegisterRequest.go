d9d41531b34b4e19e68b303113a43f4674705f27
Luisfc68 <luisfc68@gmail.com>
2023-10-02 12:50:09 -0300

    remove unused fields

--- types/RegisterRequest.go
+++ types/RegisterRequest.go
@@ -2,7 +2,6 @@ package types
 
 type ProviderRegisterRequest struct {
 	Name         string `json:"name" bson:"name" example:"New Provider" description:"Provider Name"`
-	Fee          uint   `json:"fee" bson:"fee" example:"100" description:"Fee in wei"`
 	ApiBaseUrl   string `json:"apiBaseUrl" bson:"apiBaseUrl" example:"https://api.example.com" description:"API base URL"`
 	ProviderType string `json:"providerType" bson:"providerType" example:"pegin" description:"Provider type must be \"pegin\", \"pegout\" or \"both\""`
 	Status       bool   `json:"status" bson:"status" example:"true" description:"Provider status"`
